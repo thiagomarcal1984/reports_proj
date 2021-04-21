930d29aca78d6e2840cafd97f305daa124906317
Thiago Marçal <thiagomarcal1984@gmail.com>
2021-04-21 18:19:21 -0300

    first view and template

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request):
+    hello = 'Hello world from the view.'
+    return render(request, 'sales/main.html', {'hello' : hello})
