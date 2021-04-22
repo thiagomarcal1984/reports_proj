9810721dc785af9ee4a7186b0758a837ce2bf9fa
Thiago Marçal <thiagomarcal1984@gmail.com>
2021-04-22 17:33:07 -0300

    add the report form to the modal

--- sales/views.py
+++ sales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+from reports.forms import ReportForm
 import pandas as pd
 from .utils import get_customer_from_id, get_salesman_from_id, get_chart
 
@@ -13,7 +14,8 @@ def home_view(request):
     merged_df = None
     df = None
     chart = None
-    form = SalesSearchForm(request.POST or None)
+    search_form = SalesSearchForm(request.POST or None)
+    report_form = ReportForm()
     
     if request.method == 'POST':
         date_from = request.POST.get('date_from')
@@ -59,7 +61,8 @@ def home_view(request):
             print('No Data.')
 
     context = {
-        'form':form,
+        'search_form':search_form,
+        'report_form':report_form,
         'sales_df': sales_df,
         'positions_df': positions_df,
         'merged_df': merged_df,
