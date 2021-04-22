89c894f7c5c61f8fc30dbc997cd32f63e9431063
Thiago Marçal <thiagomarcal1984@gmail.com>
2021-04-21 21:29:17 -0300

    working on the charts part 1

--- sales/utils.py
+++ sales/utils.py
@@ -1,6 +1,8 @@
-import uuid
+import uuid, base64
 from customers.models import Customer
 from profiles.models import Profile
+from io import BytesIO
+import matplotlib.pyplot as plt
 
 def generate_code():
     code = str(uuid.uuid4()).replace('-', '').upper()[:12]
@@ -13,3 +15,18 @@ def get_salesman_from_id(val):
 def get_customer_from_id(val):
     customer = Customer.objects.get(id=val)
     return customer
+
+def get_graph():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+def get_chart():
+    #...
+    chart = get_graph()
+    return chart
