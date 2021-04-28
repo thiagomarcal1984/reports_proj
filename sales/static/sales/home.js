3e1e7ed729f4550e30bb39e5c42786539de58d11
Thiago Marçal <thiagomarcal1984@gmail.com>
2021-04-28 00:12:07 -0300

    create report objects

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -3,6 +3,13 @@ console.log('Hello, world.')
 const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
 const modalBody = document.getElementById('modal-body')
+const reportForm = document.getElementById('report-form')
+
+const reportName = document.getElementById('id_name')
+const reportRemarks = document.getElementById('id_remarks')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+console.log(reportName)
+console.log(reportRemarks)
 
 console.log(reportBtn)
 console.log(img)
@@ -15,4 +22,29 @@ reportBtn.addEventListener('click', ()=> {
     console.log('clicked.')
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
+
+    console.log(img.src)
+
+    reportForm.addEventListener('submit', e=>{
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('csrfmiddlewaretoken', csrf)
+        formData.append('name', reportName.value)
+        formData.append('remarks', reportRemarks.value)
+        formData.append('image', img.src)
+
+        $.ajax({
+            type: 'POST',
+            url: '/reports/save/',
+            data: formData,
+            success: function(response){
+                console.log(response)
+            },
+            error: function(error){
+                console.log(error)
+            },
+            processData: false,
+            contentType: false,
+        })
+    })
 })
